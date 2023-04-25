0a03b51d895c10f75aa82b3dff28461b3c410394
Chan <ieejo716@naver.com>
2023-04-25 12:31:12 +0900

    #7 - 도메인클래스 설계
    각 도메인에 들어갈 메타데이터

---
+++ NoticeBoard/src/main/java/com/example/noticeboard/domain/MetaData.java
@@ -0,0 +1,10 @@
+package com.example.noticeboard.domain;
+
+import java.time.LocalDateTime;
+
+public class MetaData {
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
