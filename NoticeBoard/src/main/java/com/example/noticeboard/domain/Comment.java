98ef754fbda88bb013ee96a599d692a715f5414f
Chan <ieejo716@naver.com>
2023-04-27 01:43:11 +0900

    #10 Comment
    Comment Entity

--- NoticeBoard/src/main/java/com/example/noticeboard/domain/Comment.java
+++ NoticeBoard/src/main/java/com/example/noticeboard/domain/Comment.java
@@ -1,8 +1,73 @@
 package com.example.noticeboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Entity
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
     private Article article;
-    private User user;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    private String modifiedBy;
+
+    protected Comment() {
+
+    }
+
+    private Comment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static Comment of(Article article, String content) {
+        return new Comment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return id.equals(comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
