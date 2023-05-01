8ceedc00b7da1dd7ff592b70aee5fe675bf9db5b
Chan <ieejo716@naver.com>
2023-05-01 19:18:40 +0900

    #16 - 로그인 컨트롤러 테스트 반영
    @Import SecurityConfig을 추가하여 '/login' 컨트롤러로 테스트

--- NoticeBoard/src/test/java/com/example/noticeboard/controller/ArticleControllerTest.java
+++ NoticeBoard/src/test/java/com/example/noticeboard/controller/ArticleControllerTest.java
@@ -1,15 +1,18 @@
 package com.example.noticeboard.controller;
 
+import com.example.noticeboard.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Import(SecurityConfig.class)
 @DisplayName("View Controller - 게시글")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
