c2ccfcbb4c8b1ab93dd10e9bafd63305d392debf
Chan <ieejo716@naver.com>
2023-05-01 12:41:00 +0900

    #14 - QueryDSL 사용
    - QueryDSL을 사용하여 검색 기능을 추가 (게시글,댓글)

--- NoticeBoard/src/main/java/com/example/noticeboard/repository/ArticleRepository.java
+++ NoticeBoard/src/main/java/com/example/noticeboard/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.example.noticeboard.repository;
 
 import com.example.noticeboard.domain.Article;
+import com.example.noticeboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        //todo QuerydslPredicateExecutor를 통해 모든 field들에 대한 검색이 가능하다.
+        // 이러한 내용을 custom하기 위한 메서드
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- NoticeBoard/src/main/java/com/example/noticeboard/repository/CommentRepository.java
+++ NoticeBoard/src/main/java/com/example/noticeboard/repository/CommentRepository.java
@@ -1,9 +1,29 @@
 package com.example.noticeboard.repository;
 
 import com.example.noticeboard.domain.Comment;
+import com.example.noticeboard.domain.QArticle;
+import com.example.noticeboard.domain.QComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository
+        extends JpaRepository<Comment, Long>,
+        QuerydslPredicateExecutor<Comment>,
+        QuerydslBinderCustomizer<QComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        //todo QuerydslPredicateExecutor를 통해 모든 field들에 대한 검색이 가능하다.
+        // 이러한 내용을 custom하기 위한 메서드
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
