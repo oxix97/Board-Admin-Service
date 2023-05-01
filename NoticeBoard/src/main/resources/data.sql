4956ecf8855f6d122b2e6549aa8d2beec7181b6b
Chan <ieejo716@naver.com>
2023-05-01 10:57:55 +0900

    #14 - data.sql
    domain에 추가한 내용이 잘 작동하는지 확인하기 위한 sql

---
+++ NoticeBoard/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into article (id, title, content, created_by, modified_by, created_at, modified_at,hashtag)
+values (1, 'Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+#pink', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50','#test1')
+        ,
+       (2, 'Morbi ut odio.', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
+Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
+#purple', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54','#test2');
