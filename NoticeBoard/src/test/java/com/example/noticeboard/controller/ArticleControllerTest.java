152c676f49cf7d19e1ad363c9ac0437c16cd0c2d
Chan <ieejo716@naver.com>
2023-05-01 16:54:55 +0900

    #16 - 게시글 관련 뷰 테스트 코드
    게시글 뷰에서 정상적으로 역할을 하는지 확인하는 테스트코드 수정

---
+++ NoticeBoard/src/test/java/com/example/noticeboard/controller/ArticleControllerTest.java
@@ -0,0 +1,71 @@
+package com.example.noticeboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Disabled
+@DisplayName("View Controller - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[GET] 게시글 리스트 - 정상 호출")
+    @Test
+    void test1() throws Exception {
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[GET] 게시글 단건[1] - 정상 호출")
+    @Test
+    void test2() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[GET] 게시글 상세페이지 - 정상 호출")
+    @Test
+    void test3() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles/comments"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    void test4() throws Exception {
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    void test5() throws Exception {
+        mvc.perform(get("/articles/hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+}
