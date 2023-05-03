9a38b0a35738c7bfd811d80092e62ad93c030276
Chan <ieejo716@naver.com>
2023-05-03 23:02:46 +0900

    #19  - 댓글 Service
    게시판 댓글에 대한 비즈니스 로직 담당

--- NoticeBoard/src/main/java/com/example/noticeboard/service/CommentService.java
+++ NoticeBoard/src/main/java/com/example/noticeboard/service/CommentService.java
@@ -1,4 +1,30 @@
 package com.example.noticeboard.service;
 
+import com.example.noticeboard.domain.Article;
+import com.example.noticeboard.domain.Comment;
+import com.example.noticeboard.dto.ArticleDto;
+import com.example.noticeboard.dto.CommentDto;
+import com.example.noticeboard.repository.ArticleRepository;
+import com.example.noticeboard.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommentDto> searchComments(Long articleId) {
+        Stream<Comment> comments = articleRepository.findById(articleId).orElseThrow()
+                .getComments().stream();
+        return comments.map(CommentDto::from).toList();
+    }
 }
