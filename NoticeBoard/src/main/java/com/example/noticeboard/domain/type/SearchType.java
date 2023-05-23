0463d0b5883032aed7a04363d5df5051013948c6
Chan <ieejo716@naver.com>
2023-05-24 00:54:28 +0900

    #19 검색 타입 변경
    검색 타입에 부가적으로 내용을 추가

--- NoticeBoard/src/main/java/com/example/noticeboard/domain/type/SearchType.java
+++ NoticeBoard/src/main/java/com/example/noticeboard/domain/type/SearchType.java
@@ -1,9 +1,14 @@
 package com.example.noticeboard.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
-    TITLE,
-    CONTENT,
-    ID,
-    NICKNAME,
-    HASHTAG
+    TITLE("제목"), CONTENT("본문"), ID("유저 ID"), NICKNAME("닉네임"), HASHTAG("해시태그");
+
+    @Getter
+    private final String value;
+
+    SearchType(String value) {
+        this.value = value;
+    }
 }
