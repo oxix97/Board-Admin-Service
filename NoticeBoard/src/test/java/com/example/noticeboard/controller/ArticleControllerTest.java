64c2997eb9084f2c58bb81334d268cbc65ac79de
Chan <ieejo716@naver.com>
2023-05-27 06:15:20 +0900

    #21 인증 기능을 추가한 게시판 서비스 테스트
    각 게시판 서비스 기능에 인가된 사용자만 접근해야 할 로직을 추가

--- NoticeBoard/src/test/java/com/example/noticeboard/controller/ArticleControllerTest.java
+++ NoticeBoard/src/test/java/com/example/noticeboard/controller/ArticleControllerTest.java
@@ -1,9 +1,14 @@
 package com.example.noticeboard.controller;
 
-import com.example.noticeboard.config.SecurityConfig;
+import com.example.noticeboard.config.TestSecurityConfig;
+import com.example.noticeboard.domain.type.FormStatus;
 import com.example.noticeboard.domain.type.SearchType;
-import com.example.noticeboard.dto.*;
+import com.example.noticeboard.dto.ArticleDto;
+import com.example.noticeboard.dto.ArticleWithCommentDto;
+import com.example.noticeboard.dto.HashtagDto;
+import com.example.noticeboard.dto.UserAccountDto;
 import com.example.noticeboard.dto.request.ArticleRequest;
+import com.example.noticeboard.dto.response.ArticleResponse;
 import com.example.noticeboard.service.ArticleService;
 import com.example.noticeboard.service.PaginationService;
 import com.example.noticeboard.util.FormDataEncoder;
@@ -16,6 +21,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -28,7 +36,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import({SecurityConfig.class, FormDataEncoder.class})
+@Import({TestSecurityConfig.class, FormDataEncoder.class})
 @DisplayName("View Controller - 게시글")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
@@ -49,7 +57,7 @@ class ArticleControllerTest {
         this.formDataEncoder = formDataEncoder;
     }
 
-    @DisplayName("[GET] 게시글 리스트 - 정상 호출")
+    @DisplayName("[GET] 게시글 리스트 (인증된 사용자)- 정상 호출")
     @Test
     void test1() throws Exception {
         //given
@@ -66,13 +74,16 @@ class ArticleControllerTest {
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
-    @DisplayName("[GET] 게시글 상세페이지 - 정상 호출")
+    // @WithMockUser todo Security 인증 통과시키는 어노테이션
+    //todo 테스트 유저를 가지고 인증
+    @WithUserDetails(value = "security-test", setupBefore = TestExecutionEvent.TEST_EXECUTION, userDetailsServiceBeanName = "userDetailsService")
+    @DisplayName("[GET] 게시글 상세페이지 (인증된 사용자)- 정상 호출")
     @Test
     void test3() throws Exception {
         Long id = 1L;
+        long totalCount = 1L;
         //given
-        given(articleService.getArticle(id)).willReturn(createArticleWithCommentsDto());
-
+        given(articleService.getArticleWithComments(id)).willReturn(createArticleWithCommentsDto());
         //when
         mvc.perform(get("/articles/" + id))
                 .andExpect(status().isOk())
@@ -82,8 +93,22 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("comments"));
 
         //then
-        then(articleService).should().getArticle(id);
+        then(articleService).should().getArticleWithComments(id);
 
+    }
+
+    @DisplayName("[GET] 게시글 상세페이지 (인증되지 않은 사용자 -> 로그인 페이지 이동)")
+    @Test
+    void test31() throws Exception {
+        //given
+        long articleId = 1L;
+        //when
+        mvc.perform(get("/articles/" + articleId))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+        //then
+        then(articleService).shouldHaveNoInteractions();
+        then(articleService).shouldHaveNoInteractions();
     }
 
     @DisplayName("[GET] 게시글 검색 전용 페이지 - 정상 호출")
@@ -161,6 +186,81 @@ class ArticleControllerTest {
         then(articleService).should().searchArticlesViaHashtag(eq(hashtag), any(Pageable.class));
         then(articleService).should().getHashtags();
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @WithUserDetails(value = "security-test", setupBefore = TestExecutionEvent.TEST_EXECUTION, userDetailsServiceBeanName = "userDetailsService")
+    @DisplayName("[DELETE] 게시글 삭제 - 정상 호출")
+    @Test
+    void test41() throws Exception {
+        //given
+        long articleId = 1L;
+        String userId = "security-test";
+        willDoNothing().given(articleService).deleteArticle(articleId, userId);
+        //when
+        mvc.perform(post("/articles/" + articleId + "/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles"));
+        //then
+        then(articleService).should().deleteArticle(articleId, userId);
+    }
+
+    @WithMockUser
+    @DisplayName("[GET] 게시글 작성 페이지 - 정상 호출")
+    @Test
+    void test2() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/form"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("formStatus", FormStatus.CREATE))
+                .andExpect(view().name("articles/form"));
+
+        //then
+    }
+
+    @DisplayName("[PUT] 게시글 작성 - 정상 호출")
+    @WithUserDetails(value = "security-test", setupBefore = TestExecutionEvent.TEST_EXECUTION, userDetailsServiceBeanName = "userDetailsService")
+    @Test
+    void test21() throws Exception {
+        //given
+        ArticleDto article = createArticleDto();
+        ArticleRequest request = ArticleRequest.of("title21", "content21");
+        willDoNothing().given(articleService).saveArticle(article);
+
+        //when
+        mvc.perform(post("/articles/form")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles"))
+                .andExpect(redirectedUrl("/articles"));
+        //then
+        then(articleService).should().saveArticle(any(ArticleDto.class));
+    }
+
+    @DisplayName("[GET] 게시글 수정 페이지 - 정상 호출")
+    @WithUserDetails(value = "security-test", setupBefore = TestExecutionEvent.TEST_EXECUTION, userDetailsServiceBeanName = "userDetailsService")
+    @Test
+    void test22() throws Exception {
+        //given
+        long articleId = 1L;
+        ArticleDto article = createArticleDto();
+        given(articleService.getArticle(articleId)).willReturn(article);
+        //when
+        mvc.perform(get("/articles/form/" + articleId))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/form"))
+                .andExpect(model().attribute("article", ArticleResponse.from(article)))
+                .andExpect(model().attribute("formStatus", FormStatus.UPDATE));
+
+        //then
+        then(articleService).should().getArticle(articleId);
     }
 
     private ArticleDto createArticleDto() {
--- NoticeBoard/src/test/java/com/example/noticeboard/repository/JpaRepositoryTest.java
+++ NoticeBoard/src/test/java/com/example/noticeboard/repository/JpaRepositoryTest.java
@@ -9,15 +9,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Disabled
 @DisplayName("JPA 연결 테스트")
-@Import(JpaConfig.class)
+@Import(JpaRepositoryTest.TestJpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
     private final ArticleRepository articleRepository;
@@ -82,4 +87,13 @@ class JpaRepositoryTest {
 //        articleRepository.save(article);
 //        commentRepository.saveAndFlush(comment);
 //    }
+
+    @EnableJpaAuditing
+    @TestConfiguration
+    public static class TestJpaConfig {
+        @Bean
+        public AuditorAware<String> auditorAware() {
+            return () -> Optional.of("test1");
+        }
+    }
 }
