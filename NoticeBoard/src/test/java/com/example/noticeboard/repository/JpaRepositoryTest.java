dde0bac94948863cfeacb10ecfe2ac3015e877ce
Chan <ieejo716@naver.com>
2023-06-28 10:56:53 +0900

    #28 JpaRepositoryTest
    Jpa Repository 상속한 각 레포지토리의 기능 테스트

--- NoticeBoard/src/test/java/com/example/noticeboard/repository/JpaRepositoryTest.java
+++ NoticeBoard/src/test/java/com/example/noticeboard/repository/JpaRepositoryTest.java
@@ -1,10 +1,7 @@
 package com.example.noticeboard.repository;
 
-import com.example.noticeboard.config.JpaConfig;
 import com.example.noticeboard.domain.Article;
-import com.example.noticeboard.domain.Comment;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import com.example.noticeboard.domain.ArticleComment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,19 +16,19 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.COLLECTION;
 
-@Disabled
 @DisplayName("JPA 연결 테스트")
 @Import(JpaRepositoryTest.TestJpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
     private final ArticleRepository articleRepository;
-    private final CommentRepository commentRepository;
+    private final ArticleCommentRepository articleCommentRepository;
 
     @Autowired
-    public JpaRepositoryTest(ArticleRepository articleRepository, CommentRepository commentRepository) {
+    public JpaRepositoryTest(ArticleRepository articleRepository, ArticleCommentRepository articleCommentRepository) {
         this.articleRepository = articleRepository;
-        this.commentRepository = commentRepository;
+        this.articleCommentRepository = articleCommentRepository;
     }
 
     @DisplayName("SELECT Test")
@@ -39,9 +36,7 @@ class JpaRepositoryTest {
     void selectTest() {
         List<Article> articles = articleRepository.findAll();
         assertThat(articles)
-                .isNotNull()
-                .hasSize(0);
-
+                .isNotNull();
     }
 
 //    @DisplayName("INSERT Test")
@@ -75,18 +70,55 @@ class JpaRepositoryTest {
 //        insertData();
         Article article = articleRepository.findById(1L).orElseThrow();
         Long preArticleCount = articleRepository.count();
-        Long preCommentCount = commentRepository.count();
-        int deletedCommentSize = article.getComments().size();
+        Long preCommentCount = articleCommentRepository.count();
+        int deletedCommentSize = article.getArticleComments().size();
 
         articleRepository.delete(article);
     }
 
 //    private void insertData() {
 //        Article article = Article.of("dummy", "momo", "#dummy");
-//        Comment comment = Comment.of(article, "i'm hungry");
+//        ArticleComment comment = ArticleComment.of(article, "i'm hungry");
 //        articleRepository.save(article);
 //        commentRepository.saveAndFlush(comment);
 //    }
+
+    @DisplayName("댓글에 대한 대댓글 조회 테스트")
+    @Test
+    void test111() {
+        //given
+
+        //when
+        Optional<ArticleComment> parentComment = articleCommentRepository.findById(1L);
+
+        //then
+        assertThat(parentComment).get()
+                .hasFieldOrPropertyWithValue("parentCommentId", null)
+                .extracting("childComments", COLLECTION)
+                .hasSize(4);
+    }
+
+    @DisplayName("댓글에 대댓글 삽입 테스트")
+    @Test
+    void test112() {
+        //given
+        ArticleComment parentArticleComment = articleCommentRepository.getReferenceById(1L);
+        ArticleComment childArticleComment = ArticleComment.of(
+                parentArticleComment.getArticle(),
+                parentArticleComment.getUserAccount(),
+                "대댓글"
+        );
+
+        //when
+        parentArticleComment.addChildComment(childArticleComment);
+        articleRepository.flush();
+
+        //then
+        assertThat(articleCommentRepository.findById(1L)).get()
+                .hasFieldOrPropertyWithValue("parentCommentId", null)
+                .extracting("childComments", COLLECTION)
+                .hasSize(5);
+    }
 
     @EnableJpaAuditing
     @TestConfiguration
