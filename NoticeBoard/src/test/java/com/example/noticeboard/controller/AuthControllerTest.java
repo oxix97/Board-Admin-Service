fc6f0000416caeb2404e81cfad7dc0a321a263d3
Chan <ieejo716@naver.com>
2023-06-28 10:56:17 +0900

    #28 인증 기능 테스트
    Spring Security 기본 기능 테스트

--- NoticeBoard/src/test/java/com/example/noticeboard/controller/AuthControllerTest.java
+++ NoticeBoard/src/test/java/com/example/noticeboard/controller/AuthControllerTest.java
@@ -1,32 +1,56 @@
 package com.example.noticeboard.controller;
 
+import com.example.noticeboard.config.TestSecurityConfig;
+import com.example.noticeboard.service.ArticleService;
+import com.example.noticeboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("Auth Controller - 인증")
-@WebMvcTest
+@DisplayName("View 컨트롤러 - 인증")
+@Import(TestSecurityConfig.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 public class AuthControllerTest {
     private final MockMvc mvc;
+
+    @MockBean
+    private ArticleService articleService;
+    @MockBean
+    private PaginationService paginationService;
 
     public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @WithMockUser
     @DisplayName("[GET] : 로그인페이지 - 정상 호출")
     @Test
     void test1() throws Exception {
+
+        //when
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+
+        //then
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
+    }
+
+    @TestComponent
+    static class EmptyController {
     }
 }
