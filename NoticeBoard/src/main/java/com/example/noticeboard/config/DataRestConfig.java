bd2c1d9c79305941bec7e72d748f2bf4be9c54d4
Chan <ieejo716@naver.com>
2023-06-30 18:59:59 +0900

    #35 Swagger 실행 여부 확인을 위한 예제
    REST API를 테스트로 하나 구현

---
+++ NoticeBoard/src/main/java/com/example/noticeboard/config/DataRestConfig.java
@@ -0,0 +1,23 @@
+package com.example.noticeboard.config;
+
+import com.example.noticeboard.domain.Article;
+import com.example.noticeboard.domain.ArticleComment;
+import com.example.noticeboard.domain.Hashtag;
+import com.example.noticeboard.domain.UserAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
+        );
+    }
+}
