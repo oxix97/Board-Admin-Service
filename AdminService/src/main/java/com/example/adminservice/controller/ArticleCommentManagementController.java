700c27cad9c04d804f24a35ab5bab0fa9ffed37b
Chan <ieejo716@naver.com>
2023-07-07 12:55:23 +0900

    #51 뷰 엔드포인트 뼈대 구성

--- AdminService/src/main/java/com/example/adminservice/controller/ArticleCommentManagementController.java
+++ AdminService/src/main/java/com/example/adminservice/controller/ArticleCommentManagementController.java
@@ -1,10 +1,14 @@
 package com.example.adminservice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/management/article-comments")
 @Controller
 public class ArticleCommentManagementController {
-
+    @GetMapping
+    public String managementArticleComments() {
+        return "management/article-comments";
+    }
 }
